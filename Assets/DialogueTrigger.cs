9ef790e4ff3894e7e75d4413adf693b35dfad45c
Paul <122436202+TUP-Clover@users.noreply.github.com>
2024-01-28 02:02:08 +0800

    Added Intro dialogue and bg music (still need to edit)

---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class DialogueCharacter
+{
+    public string name;
+    public Sprite icon;
+}
+
+[System.Serializable]
+public class DialogueLine
+{
+    public DialogueCharacter character;
+    [TextArea(3, 10)]
+    public string line;
+}
+
+[System.Serializable]
+public class Dialogue
+{
+    public List<DialogueLine> dialogueLines = new List<DialogueLine>();
+}
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    private bool dialogueTriggered = false;
+
+    // Assuming you have a reference to the PlayableDirector
+    public PlayableDirector playableDirector;
+    private ActivationTrackListener activationTrackListener;
+
+    private void Start()
+    {
+        // Assuming the ActivationTrackListener script is attached to the same GameObject
+        activationTrackListener = GetComponent<ActivationTrackListener>();
+
+        if (activationTrackListener != null)
+        {
+            activationTrackListener.OnActivationFinished += OnActivationFinished;
+        }
+    }
+
+    private void OnActivationFinished()
+    {
+        if (!dialogueTriggered)
+        {
+            TriggerDialogue();
+            dialogueTriggered = true;
+        }
+    }
+
+    public void TriggerDialogue()
+    {
+        DialogueManager.Instance.StartDialogue(dialogue);
+    }
+}
